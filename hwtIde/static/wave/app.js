0dad6467a9b396df5d5d46c7392d7a5b4bb9e0b3
nic30 <nic30@seznam.cz>
2017-03-28 01:54:00 +0200

    simple wave graph with grid background

--- hwtIde/static/wave/app.js
+++ hwtIde/static/wave/app.js
@@ -1,9 +1,36 @@
-function random(){
-	return Math.random() > 0.5
+var n = 40
+
+function randData(){
+	function random(){
+		var r = Math.random()
+		if (r < 0.4)
+			return false
+		else if (r>0.6)
+			return true
+		else 
+			return null
+	}
+	var data = []
+	var lastD = null
+	for (var i = 0; i< n; i++){
+		var d = random()
+		// we need to create points when value changes
+		// to have right angle curve
+		if (d != lastD && i != 0){
+			data.push([i, lastD])
+		}
+		lastD = d
+		data.push([i, d])
+	}
+	
+	return data
 }
 
-var n = 40, 
-    data = d3.range(n).map(random);
+var signalData = []
+for (var i =0; i< 30; i++){
+	signalData.push(["clk"+i, randData()])
+}
+
 var svg = d3.select("svg"), 
     margin = {
 		top : 20,
@@ -11,64 +38,118 @@ var svg = d3.select("svg"),
 		bottom : 20,
 		left : 40
 	}, 
-	width = svg.attr("width") - margin.left - margin.right,
-	height = svg.attr("height")	- margin.top - margin.bottom, 
+	width = parseInt(svg.style('width')) - margin.left - margin.right,
+	height = parseInt(svg.style("height"))	- margin.top - margin.bottom, 
 	g = svg.append("g")
 		   .attr("transform",
 		         "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleLinear()
-          .domain([ 0, n - 1 ])
-          .range([ 0, width ]);
-var y = d3.scaleLinear()
-          .domain([ 0, 1 ])
-          .range([ height, 0 ]);
-var line = d3.line()
-             .x(function(d, i) {
-                  return x(i);
-              })
-             .y(function(d, i) {
-             	return y(d);
-              });
+		.domain([ 0, n - 1 ])
+		.range([ 0, width ]);
 
-g.append("defs")
- .append("clipPath")
- .attr("id", "clip")
- .append("rect")
- .attr("width", width)
- .attr("height", height);
-
+// x axis
 g.append("g")
  .attr("class", "axis axis--x")
- .attr("transform", "translate(0," + y(0) + ")")
+ .attr("transform", "translate(0," + height + ")")
  .call(d3.axisBottom(x));
 
-//g.append("g")
-// .attr("class", "axis axis--y")
-// .call(d3.axisLeft(y));
 
+var signalNames = signalData.map(function(d){
+ 	return d[0]
+})
+
+//var y = d3.scaleBand()
+//          .rangeRound([0, height])
+//          .padding(0.1)
+//          .domain(signalNames)
+//
+//const nameScale = d3.scaleOrdinal()
+//                    .domain(signalNames)
+//                    .range([height, 0]);
+var y = d3.scaleLinear()
+          .domain([0,1])
+          .range([height, 0])
+
+// y axis
 g.append("g")
- .attr("clip-path", "url(#clip)")
- .append("path")
- .datum(data)
- .attr("class", "line")
- .transition()
- .duration(250)
- .ease(d3.easeLinear)
- .on("start", tick);
+ .attr("class", "axis axis--y")
+ .call(d3.axisLeft(y))
 
-function tick() {
-	// Push a new data point onto the back.
-	data.push(random());
-	// Redraw the line.
-	d3.select(this)
-	  .attr("d", line)
-	  .attr("transform", null);
-	// Slide it to the left.
-	d3.active(this)
-	  .attr("transform", "translate(" + x(-1) + ",0)")
-	  .transition()
-	  .on("start", tick);
-	// Pop the old data point off the front.
-	data.shift();
+function renderWaveRow(indx, data){
+	var waveRowWidth = width
+	var waveRowHeight = 20
+	var waveRowYpadding = 5
+
+	var waveRowX =  d3.scaleLinear()
+					  .domain([ 0, n - 1 ])
+					  .range([ 0, waveRowWidth ]);
+
+	var waveRowY = d3.scaleLinear()
+	                 .domain([0, 1])
+	                 .range([(waveRowHeight + waveRowYpadding) * (indx + 1) - waveRowYpadding,
+	                	     (waveRowHeight + waveRowYpadding) * indx]);
+
+	var line = d3.line()
+	   .x(function(d) {
+	        return waveRowX(d[0]);
+	    })
+	   .y(function(d) {
+	   		return waveRowY(d[1]);
+	    });
+	
+	// wave line
+	g.append("g")
+	 .attr("clip-path", "url(#clip)")
+	 .append("path")
+	 .datum(data)
+	 .attr("class", "line")
+	 .attr("d", line)
+
+	// Add the scatterplot
+	g.selectAll("invalid-rect")	
+	    .data(data)
+	    .enter()
+	    .filter(function(d, indx) { return d[1] === null && (indx===0 || data[indx-1][1] !== null) })
+	    .append("rect")						
+	    .attr("height", waveRowHeight)
+	    .attr("width", waveRowWidth/n)
+	    .style("fill",  "red")
+	    .attr("x", function(d) { return waveRowX(d[0]) })		 
+	    .attr("y", function(d) { return waveRowY(0)- waveRowHeight }); 
+	
 }
+signalData.forEach(function(data, indx){
+	renderWaveRow(indx, data[1])
+})
+
+
+// imple graph with grid lines in v4 https://bl.ocks.org/d3noob/c506ac45617cf9ed39337f99f8511218
+function make_x_gridlines() {		
+    return d3.axisBottom(x)
+             .ticks(n)
+}
+
+//// gridlines in y axis function
+//function make_y_gridlines() {		
+//    return d3.axisLeft(y)
+//        	 .ticks(signalData[0][1].length)
+//}
+
+// add the X gridlines
+g.append("g")			
+    .attr("class", "grid")
+    .attr("transform", "translate(0," + height + ")")
+    .call(make_x_gridlines()
+        .tickSize(-height)
+        .tickFormat("")
+    )
+
+//// add the Y gridlines
+//g.append("g")			
+//    .attr("class", "grid")
+//    .call(make_y_gridlines()
+//        .tickSize(-width)
+//        .tickFormat("")
+//    )
+//
