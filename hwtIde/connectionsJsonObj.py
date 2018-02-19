b5af55bfee484725ed29cfa7319b34e95f33dbb8
Nic30 <nic30@seznam.cz>
2018-02-19 18:27:35 +0100

    partial update of layout data containers

--- hwtIde/connectionsJsonObj.py
+++ hwtIde/connectionsJsonObj.py
@@ -1,35 +1,48 @@
 from flask.wrappers import Response
+from itertools import chain
 import json
 import os
 from stat import S_ISDIR
 import time
 from typing import List
 
-from hwt.hdl.constants import INTF_DIRECTION, DIRECTION
+from hwt.hdl.constants import INTF_DIRECTION, DIRECTION, DIRECTION_to_str
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwt.synthesizer.unit import Unit
 import xml.etree.ElementTree as etree
 
+
 UNIT_HEADER_OFFSET = 20
 PORT_HEIGHT = 20
+PORT_X_PADDING = 20
+UNIT_PADDING = 5
+
+
+def width_of_str(s):
+    return len(s) * 5
 
 
 class IdCtx(dict):
     def __init__(self, *args, **kwargs):
         dict.__init__(self, *args, **kwargs)
         self.max_id = 0
+        self.orig_to_layout = {}
 
-    def register(self, obj) -> int:
+    def register(self, origObj, layoutObj) -> int:
+        assert origObj not in self.orig_to_layout, origObj
         i = self.max_id
-        self[i] = obj
+        self[i] = layoutObj
+        self.orig_to_layout[origObj] = layoutObj
         self.max_id += 1
         return i
 
 
 def jsonResp(data):
-    return Response(response=json.dumps(data, default=_defaultToJson),
-                    status=200, mimetype="application/json")
+    return Response(response=json.dumps(
+        data,
+        default=_defaultToJson),
+        status=200, mimetype="application/json")
 
 
 def _defaultToJson(obj):
@@ -41,7 +54,7 @@ def _defaultToJson(obj):
 class GeometryRect():
     def __init__(self, x, y, width, height):
         self.x = x
-        self.y = self.y
+        self.y = y
         self.width = width
         self.height = height
 
@@ -92,17 +105,9 @@ class LayoutPort():
         self.geometry = None
         self.id = id_ctx.register(self)
 
-    def updateGeometry(self):
-        p = self.parent.geometry
-        if self.direction == INTF_DIRECTION.SLAVE:
-            x = 0
-        else:
-            x = p.x + p.width / 2
-
-        y = p.y + UNIT_HEADER_OFFSET + self.on_parent_index * PORT_HEIGHT
-        width = p.width / 2
-        height = PORT_HEIGHT
-        self.geometry = GeometryRect(x, y, width, height)
+    def updateGeometry(self, x_offset, y_offset, width):
+        y = y_offset + UNIT_HEADER_OFFSET + self.on_parent_index * PORT_HEIGHT
+        self.geometry = GeometryRect(x_offset, y, width, PORT_HEIGHT)
 
     def toJson(self):
         return {"id": self.id,
@@ -118,17 +123,38 @@ class LayoutUnit():
     :ivar class_name: name of class of this unit
     :ivar inputs: list of LayoutPort for each input of this unit
     :ivar outputs: list of LayoutPort for each output of this unit
-    :ivar _id_ctx: id context for resolution of unique ids for each diagram object
+    :ivar _id_ctx: id context for resolution of unique ids
+        for each diagram object
     :ivar id: unique diagram id of this object
     """
 
-    def __init__(self, id_ctx: IdCtx, name: str, class_name: str):
+    def __init__(self, id_ctx: IdCtx, unit: Unit, name: str, class_name: str):
         self.name = name
         self.class_name = class_name
         self.inputs = []
         self.outputs = []
         self._id_ctx = id_ctx
-        self.id = id_ctx.register(self)
+        self.id = id_ctx.register(unit, self)
+        self.geometry = None
+
+    def updateGeometry(self, x, y):
+        label_w = width_of_str(self.name)
+        port_w = max(*map(lambda p: width_of_str(p.name),
+                          chain(self.inputs, self.outputs)),
+                     label_w / 2)
+        width = max(port_w, label_w)
+        height = UNIT_HEADER_OFFSET + \
+            max(len(self.inputs), len(self.outputs)) * PORT_HEIGHT
+
+        port_width = width / 2
+        for i in self.inputs:
+            i.updateGeometry(x, y, port_width)
+
+        o_x_offset = x + port_width
+        for o in self.outputs:
+            o.updateGeometry(o_x_offset, y, port_width)
+
+        self.geometry = GeometryRect(x, y, width, height)
 
     def add_port(self, intf: Interface):
         if intf._direction == INTF_DIRECTION.MASTER:
@@ -138,12 +164,14 @@ class LayoutUnit():
         else:
             raise Exception()
 
-        p = LayoutPort(self._id_ctx, intf, len(portArr), intf, intf._direction)
+        p = LayoutPort(self._id_ctx, self,
+                       len(portArr), intf._name,
+                       intf._direction)
         portArr.append(p)
 
     @classmethod
     def fromIntfUnit(cls, id_ctx: IdCtx, u: Unit):
-        self = cls(id_ctx, u._name, u.__class__.__name__)
+        self = cls(id_ctx, u, u._name, u.__class__.__name__)
         for intf in u._interfaces:
             self.add_port(intf)
 
@@ -153,23 +181,32 @@ class LayoutUnit():
         raise self.id + 2
 
     def toMxGraph(self):
-        raise NotImplementedError()
+        c = mxCell(
+            value=self.name,
+            style="rounded=0;whiteSpace=wrap;html=1;",
+            parent=1, vertex=1)
+        c.append(self.geometry.toMxGraph())
+        return c
 
     def toJson(self):
-        d = {"name": self.name,
-             "id": self.id,
-             "isExternalPort": False,
-             "geometry": self.geometry.toJson(),
-             "inputs":  self.inputs,
-             "outputs": self.outputs}
-        return d
+        return {"name": self.name,
+                "id": self.id,
+                "isExternalPort": False,
+                "geometry": self.geometry.toJson(),
+                "inputs":  [i.toJson()
+                            for i in self.inputs],
+                "outputs": [o.toJson()
+                            for o in self.outputs]
+                }
 
 
 class LayoutExternalPort(LayoutUnit):
     def __init__(self, id_ctx: IdCtx, intf):
         super(LayoutExternalPort, self).__init__(
-            id_ctx, intf._name, intf.__class__.__name__)
+            id_ctx, intf._sigInside,
+            intf._name, intf.__class__.__name__)
         self.add_port(intf)
+        self.direction = intf._direction
 
     def toMxGraph(self):
         c = mxCell(id=self.getMxGraphId(),
@@ -182,16 +219,18 @@ class LayoutExternalPort(LayoutUnit):
 
     def toJson(self):
         j = super(LayoutExternalPort, self).toJson()
-        j["direction"] = DIRECTION.opposite(
-            INTF_DIRECTION.asDirection(self.origin._direction))
+        j["direction"] = DIRECTION_to_str[DIRECTION.opposite(
+            INTF_DIRECTION.asDirection(self.direction))]
         j["isExternalPort"] = True
         return j
 
 
 class LayoutNet():
-    def __init__(self, id_ctx: IdCtx, source: LayoutPort,
+    def __init__(self, id_ctx: IdCtx,
+                 signal: RtlSignal,
+                 source: LayoutPort,
                  endpoints: List[LayoutPort], name=None):
-        self.id = id_ctx.register(self)
+        self.id = id_ctx.register(signal, self)
         self.name = name
         self.source = source
         self.endpoints = endpoints
@@ -246,8 +285,18 @@ class Layout():
         self.nodes.append(n)
 
     def add_net(self, s: RtlSignal):
-        n = LayoutNet(self.id_ctx, s, [e for e in s.endpoints], name=s._name)
+        o_to_l = self.id_ctx.orig_to_layout
+        n = LayoutNet(self.id_ctx,
+                      o_to_l[s],
+                      [o_to_l[e] for e in s.endpoints],
+                      name=s.name)
         self.nets.append(n)
+
+    def resolveGeometry(self):
+        offset = 0
+        for n in self.nodes:
+            n.updateGeometry(offset, 0)
+            offset += 200
 
     def toMxGraph(self):
         gm = etree.Element("mxGraphModel")
