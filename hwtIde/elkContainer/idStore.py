3917f2cf486c905a0d6fddc9057a0a1613dd7f11
Nic30 <nic30@seznam.cz>
2018-04-09 16:17:10 +0200

    idStore explicit registration

--- hwtIde/elkContainer/idStore.py
+++ hwtIde/elkContainer/idStore.py
@@ -1,36 +1,28 @@
 from elkContainer.lNode import LNode
+from elkContainer.lPort import LPort
 
 
 class ElkIdStore(dict):
+    """
+    :attention: First register nodes then register ports
+        otherwise id will not be generated correctly
+    """
     def __init__(self, *args, **kwargs):
         dict.__init__(self, *args, **kwargs)
         self.reverseDict = {v: k for k, v in self.items()}
         self.nodeCnt = 0
+        self.portCnt = 0
 
-    def __getitem__(self, key):
-        try:
-            return dict.__getitem__(self, key)
-        except KeyError:
-            pass
+    def registerNode(self, node: LNode):
+        k = self.nodeCnt
+        self[node] = k
+        assert k not in self.reverseDict, (node, k)
+        self.reverseDict[k] = node
+        self.nodeCnt += 1
 
-        if isinstance(key, LNode):
-            name = self.nodeCnt
-            self.nodeCnt += 1
-        else:
-            try:
-                name = key.name
-            except AttributeError:
-                name = None
-
-            if name is None:
-                name = len(self)
-
-        if name in self.reverseDict:
-            name = "%s_%d" % (name, id(key))
-
-        if name in self.reverseDict:
-            raise AssertionError("Redifinition", name,
-                                 self.reverseDict[name], key)
-        self[key] = name
-        self.reverseDict[name] = key
-        return name
+    def registerPort(self, port: LPort):
+        k = self.nodeCnt + self.portCnt
+        self[port] = k
+        assert k not in self.reverseDict, (port, k)
+        self.reverseDict[k] = port
+        self.portCnt += 1
